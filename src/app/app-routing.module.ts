0808cedd79c2d03227a75e978df0b76f9df196ad
khouloud <khouloudmhamdi7@gmail.com>
2023-02-23 01:32:05 +0100

    contact and intro section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ContactComponent } from './components/contact/contact.component';
 
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { AproposPageComponent } from './components/apropos-page/apropos-page.component';
@@ -10,7 +10,8 @@ const routes: Routes = [
   {path : "Apropos" , component:AproposPageComponent},
   {path : "events" , component:EvenementsPageComponent},
   {path : "" , component:AccueilComponent},
-  
+  {path : "Contact" , component:ContactComponent},
+
 
 ];
 
